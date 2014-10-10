b1ddc2e3afbf341d25f3d8695f70a463d45eb4ab
hydai <z54981220@gmail.com>
2014-10-10 11:21:55 -0400

    Update testcase & modify the print messages

--- HW1/1-1-UserModeMonitor/UserModeMonitor.c
+++ HW1/1-1-UserModeMonitor/UserModeMonitor.c
@@ -58,154 +58,104 @@ void genSignalMessage(int status) {
         printf("\tReason:\tHangup detected on controlling terminal or death of controlling process\n");
         break;
     case SIGINT:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGINT\n");
         printf("\tReason:\tInterrupt from keyboard\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGQUIT:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGQUIT\n");
         printf("\tReason:\tQuit from keyboard\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGILL:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGILL\n");
         printf("\tReason:\tQuit from keyboard\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGABRT:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGABRT\n");
         printf("\tReason:\tAbort SIGNAL from abort(3)\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGFPE:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGFPE\n");
         printf("\tReason:\tFloating point exception\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGSEGV:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGSEGV\n");
         printf("\tReason:\tInvalid memory reference\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGPIPE:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGPIPE\n");
         printf("\tReason:\tBroken pipe: write to pipe with no readers\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGALRM:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGALRM\n");
         printf("\tReason:\tTimer SIGNAL from alarm(2)\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGTERM:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGTERM\n");
         printf("\tReason:\tTermination SIGNAL\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGUSR1:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGUSR1\n");
         printf("\tReason:\tUser-defined SIGNAL 1\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGUSR2:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGUSR2\n");
         printf("\tReason:\tUser-defined SIGNAL 2\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGCONT:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGCONT\n");
         printf("\tReason:\tContinue if stopped\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGTSTP:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGTSTP\n");
         printf("\tReason:\tStop typed at terminal\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGTTIN:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGTTIN\n");
         printf("\tReason:\tTerminal input for background process\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGTTOU:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGTTOU\n");
         printf("\tReason:\tTerminal output for background process\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGBUS:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGBUS\n");
         printf("\tReason:\tBus error (bad memory access)\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGPROF:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGPROF\n");
         printf("\tReason:\tProfiling timer expired\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGSYS:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGSYS\n");
         printf("\tReason:\tBad argument to routine (SVr4)\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGTRAP:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGTRAP\n");
         printf("\tReason:\tTrace/breakpoint trap\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGURG:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGURG\n");
         printf("\tReason:\tUrgent condition on socket (4.2BSD)\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGVTALRM:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGVTALRM\n");
         printf("\tReason:\tVirtual alarm clock (4.2BSD)\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGXCPU:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGXCPU\n");
         printf("\tReason:\tCPU time limit exceeded (4.2BSD)\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGXFSZ:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGXFSZ\n");
         printf("\tReason:\tFile size limit exceeded (4.2BSD)\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGIO:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGIO\n");
         printf("\tReason:\tI/O now possible (4.2BSD)\n");
-        printf("Child process execution FAILED\n");
         break;
     case SIGWINCH:
-        printf("=======Error Detected=======\n");
         printf("CHILD SIGNAL: SIGWINCH\n");
         printf("\tReason:\tWindow resize SIGNAL (4.3BSD, Sun)\n");
-        printf("Child process execution FAILED\n");
         break;
     default:
         printf("UNKNOWN SIGNAL ERROR\n");
