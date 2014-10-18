f4c2d4dc0651479b62f6f530190e7c0fbf66cbde
hydai <z54981220@gmail.com>
2014-10-18 11:50:29 -0400

    Test sys_fork

--- HW1/1-2-KernelObject/myforkModule.c
+++ HW1/1-2-KernelObject/myforkModule.c
@@ -1,6 +1,8 @@
 #include <linux/module.h>
+#include <linux/kthread.h>
 #include <linux/sched.h>
 #include <linux/pid.h>
+#include <linux/syscalls.h>
 
 
 MODULE_LICENSE("GPL");
@@ -8,38 +10,43 @@ MODULE_LICENSE("GPL");
 static char *inputFile;
 module_param(inputFile, charp, S_IRUGO|S_IWUSR);
 
-int my_monitor(void *argc)
+struct task_struct *task;
+
+int my_monitor(void *argv)
 {
+    //do_fork();
+    pid_t pid = sys_fork();
 	/* fork a process here by using do_fork */
 
+    if (pid == 0) {
+        printk("CHILD\n");
+    } else {
+        printk("PARENT pid = %d\n", pid);
+    }
 	/* execute program */
 
 	/* Check signal */
-
+	return 0;
 }
 
 static int __init kernel_object_test_init(void)
 {
     int result, result1;
     struct pid *kpid;
-    struct task_struct *task;
     struct sched_param param;
 
-	/* Write your Code here */
-
     printk("parameter: %s\n", inputFile);
-
-    /* create a kernel thread to run my_fork */
+    task = kthread_create(my_monitor, inputFile, "my_monitor");
+    wake_up_process(task);
 
     return 0;
 }
 
 static void __exit kernel_object_test_exit(void)
 {
+    kthread_stop(task);
     printk("<0>Remove the module\n");
 }
 
 module_init(kernel_object_test_init);
 module_exit(kernel_object_test_exit);
-
-
