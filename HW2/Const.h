2829319cfff3ede20d2812c5f2488d5569186bfb
hydai <z54981220@gmail.com>
2014-11-04 15:58:40 +0800

    HeroItem works

--- HW2/Const.h
+++ HW2/Const.h
@@ -22,11 +22,16 @@ const char          STYLE[] = "                                         \
                         {                                               \
                             margin: 0 0 0 10;                           \
                         }                                               \
+                        QGraphicsView                                   \
+                        {                                               \
+                            background-color: rgba(30,30,30,180);       \
+                            border-radius: 25;                          \
+                        }                                               \
                         ";
 // Windows setting
 const char          WINDOWS_TITLE[]         = "River & Frog";
-const int           WINDOWS_WIDTH_MIN       = 1440;
-const int           WINDOWS_LENGTH_MIN      = 900;
+const int           WINDOWS_WIDTH_MIN       = 1200;
+const int           WINDOWS_LENGTH_MIN      = 800;
 const int           WINDOWS_WIDTH_MAX       = 1920;
 const int           WINDOWS_LENGTH_MAX      = 1080;
 // Font setting
@@ -36,6 +41,7 @@ const int           FONT_SIZE_LABEL         = 36;
 
 // IMAGE RESOURCE PATH
 const char          BACKGROUND_PATH[]       = ":/res/bg.png";
+const int           PAINTER_WIDTH           = 100;
 
 // Const string
 const char          KEYBOARDINPUT[]         = "Keyboard Input";
--- HW2/GameItem.cpp
+++ HW2/GameItem.cpp
@@ -1,14 +1,16 @@
 #include "GameItem.h"
+#include "Const.h"
+
 QRectF GameItem::boundingRect() const {
-    return QRectF(0, 0, 200, 200);
+    return QRectF(0, 0, PAINTER_WIDTH, PAINTER_WIDTH);
 }
 
 void GameItem::paint(
                 QPainter *painter,
                 const QStyleOptionGraphicsItem *option, 
                 QWidget *widget) {
-    QPixmap image(":/res/char-small.jpg");
-    painter->drawPixmap(0, 0, 200, 200, image);
+    QPixmap image(":/res/char-small.png");
+    painter->drawPixmap(0, 0, PAINTER_WIDTH, PAINTER_WIDTH, image);
 }
 
 void GameItem::moveItem(int x, int y) {
